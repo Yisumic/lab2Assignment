43dc8d119b9387750de7ba51948354ee828f2f9a
Yisumic <35821379+Yisumic@users.noreply.github.com>
2018-10-12 22:08:16 -0600

    start

---
+++ src/lab2Assignment/ListElement.java
@@ -0,0 +1,33 @@
+package lab2Assignment;
+
+public class ListElement
+{
+	
+		
+		public ListElement next;
+		//private ListEl
+		private int data;
+		
+		
+		public ListElement()
+		{
+			this.next = null;
+			this.data = 0;
+			
+		}
+		
+		
+		public void setData(int data)
+		{
+			this.data = data;
+		}
+
+		
+		public int getData()
+		{
+			return this.data;
+		}
+}
+
+
+
