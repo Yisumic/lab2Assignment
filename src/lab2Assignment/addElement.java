8876e4a4420db3c20ef1abd283166e2b5cb0e9ae
Yisumic <35821379+Yisumic@users.noreply.github.com>
2018-10-14 17:23:55 -0600

    add and delete

---
+++ src/lab2Assignment/addElement.java
@@ -0,0 +1,18 @@
+package lab2Assignment;
+
+import java.util.LinkedList;
+
+public class addElement {
+	
+	public static void main(String[] args) {
+		
+		String list = null;
+		
+		LinkedList<String> ListElement = new LinkedList<String>();
+		
+		ListElement.add(list);
+	
+	}
+	
+
+}
---
+++ src/lab2Assignment/deleteElement.java
@@ -0,0 +1,41 @@
+package lab2Assignment;
+
+
+
+public class deleteElement {
+	
+	ListElement Delete(ListElement head, int position) {
+	  
+	    ListElement current = head;
+	    ListElement parent = null;
+	    int index = 0;
+	    
+	    while (current != null && index < position) {
+	    	
+	        parent = current;
+	        current = current.next;
+	        
+	        index++;
+	        
+	    }
+	    
+	    if (current != null && parent == null) {
+	        
+	        if (current.next != null) {
+	           
+	            head = current.next;
+	        }
+	        else {
+	            
+	            head = null;
+	        }
+	    }
+	    else if (current != null) {
+	       
+	        parent.next = current.next;
+	    }
+	    
+	    return head;
+	}
+
+}
